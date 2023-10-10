a4dd32eee1458be59ce6f6aeccdee82e6c75d77c
SeongGwangJu <seonggwangdev@gmail.com>
2023-10-10 11:42:54 +0900

    FE: signup function is added.

--- front/src/pages/Signup/Signup.jsx
+++ front/src/pages/Signup/Signup.jsx
@@ -1,16 +1,54 @@
-import React from 'react';
-
-const handleSignupSubmit = async () => {
-
-}
-
-
+import React, { useState } from 'react';
+/** @jsxImportSource @emotion/react */
+import * as S from './Style'
+import { signup } from '../../apis/api/account';
+import { useNavigate } from 'react-router-dom';
 function Signup(props) {
+
+    const navigate = useNavigate();
+
+    const emptyAccount = {
+        name: "",
+        email: "",
+        password: ""
+    }
+    const [ account, setAccount ] = useState(emptyAccount);
+    const [ errorMsg, setErrorMsg ] = useState("");
+
+    const handleInputChange = (e) => {
+        console.log(account)
+        setAccount({
+            ...account,
+            [e.target.name]: e.target.value
+        });
+    }
+    const handleSignupSubmit = async () => {
+        try{
+            await signup(account);
+            navigate("/accounts/login");
+
+        }catch(error) {
+			const responseErrorMsg = error.response.data
+			const keys = Object.keys(responseErrorMsg);
+
+            if(keys.includes("name")) {
+				setErrorMsg(responseErrorMsg.username);
+			}else if(keys.includes("email")) {
+				setErrorMsg(responseErrorMsg.phoneOrEmail);
+			}else if(keys.includes("password")) {
+				setErrorMsg(responseErrorMsg.password);
+			}
+        }
+    }
+
     return (
-        <div>
-            <input placeholder={"name"} name={"name"} />
-            <input placeholder={"email"} name={"email"} />
-            <input type={"password"} placeholder={"password"} name={"password"} />
+        <div css={S.SLayout}>
+            <div>
+                {errorMsg}
+            </div>
+            <input type="text" placeholder="name" name="name" onChange={handleInputChange} />
+            <input type="text" placeholder="email" name="email" onChange={handleInputChange} />
+            <input type="password" placeholder="password" name="password" onChange={handleInputChange} />
             <button onClick={handleSignupSubmit} >가입</button>
         </div>
     );
